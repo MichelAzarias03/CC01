99ea69d8ae604dc5aab89ccf3fb3a1e85ac6246e
MichelAzarias03 <70539665+MichelAzarias03@users.noreply.github.com>
2020-11-18 14:24:34 +0100

    3

--- CC01.DAL/Class1.cs
+++ CC01.DAL/Class1.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using CC01.BO;
+using System.IO;
+using Newtonsoft.Json;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
