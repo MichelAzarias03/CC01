fcab76a0114ab8a5c80262051b323555570cbfa8
MichelAzarias03 <70539665+MichelAzarias03@users.noreply.github.com>
2020-11-19 15:09:23 +0100

    tr

--- CC01.DAL/EcoleDAO.cs
+++ CC01.DAL/EcoleDAO.cs
@@ -1,16 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using CC01.BO;
-using System.IO;
-using Newtonsoft.Json;
-using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CC01.DAL
 {
-    public class Class1
+    class EcoleDAO
     {
     }
 }
---
+++ CC01.DAL/EtudiantDAO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using CC01.BO;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC01.DAL
+{
+    class EtudiantDAO
+    {
+        private static List<Etudiant> etudiants = new List<Etudiant>();
+
+        public EtudiantDAO()
+        {
+
+        }
+    }
+}
