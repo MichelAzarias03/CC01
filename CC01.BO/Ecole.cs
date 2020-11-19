9400f505b17e7dbf67ce80b1dc15a9ad5a45d34d
dKelvine1548 <71010229+dKelvine1548@users.noreply.github.com>
2020-11-19 08:30:05 +0100

    realisation des classes

---
+++ CC01.BO/Ecole.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC01.BO
+{
+    public class Ecole
+    {
+
+        public string Identifiant { get; set; }
+
+        public byte[] Logo { get; set; }
+
+        public string Nom { get; set; }
+
+        public Ecole(string identifiant, byte[] logo, string nom)
+        {
+            Identifiant = identifiant;
+            Logo = logo;
+            Nom = nom;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Ecole ecole &&
+                   Identifiant == ecole.Identifiant;
+        }
+
+        public override int GetHashCode()
+        {
+            return 574969646 + EqualityComparer<string>.Default.GetHashCode(Identifiant);
+        }
+    }
+}
---
+++ CC01.BO/Etudiant.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC01.BO
+{
+    [Serializable]
+    class Etudiant
+    {
+        public string Nom { get; set; }
+
+        public string Prenom {get; set;}
+
+        public string Matricule { get; set; }
+
+        public byte [] CarteEtudiant { get; set; }
+
+        public Etudiant(string nom, string prenom, string matricule, byte[] carteEtudiant)
+        {
+            Nom = nom;
+            Prenom = prenom;
+            Matricule = matricule;
+            CarteEtudiant = carteEtudiant;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Etudiant etudiant &&
+                   Matricule == etudiant.Matricule;
+        }
+
+        public override int GetHashCode()
+        {
+            return 797189699 + EqualityComparer<string>.Default.GetHashCode(Matricule);
+        }
+    }
+}
